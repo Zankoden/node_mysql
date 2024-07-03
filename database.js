2a81f57192bbadeb42bbee2d507467fa7ef42653
Zankoden <zankodits@gmail.com>
2024-07-03 19:58:01 +0545

    created .env and migrated the connections

--- database.js
+++ database.js
@@ -1,11 +1,19 @@
 import mysql from "mysql2"
+import dotenv from "dotenv"
+dotenv.config()
 
 const pool= mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password:"",
-    database: "test_database1"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
- const [rows] = await  pool.query("Select * from books")
- console.log(rows)
+async function getBooks(){
+    const [rows] = await  pool.query("Select * from books")
+
+    return rows
+}
+
+const books = await getBooks()
+console.log(books)
