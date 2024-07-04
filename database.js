eae9c0a3874a3539818ad3c04575c50eaa9482ec
Zankoden <zankodits@gmail.com>
2024-07-04 11:46:26 +0545

    made create book function with sql

--- database.js
+++ database.js
@@ -26,9 +26,33 @@ async function getBookById(id) {
 // const books = await getBooks()
 // console.log(books)
 
-const bookById = await getBookById(2)
-console.log(bookById)
+// const bookById = await getBookById(2)
+// console.log(bookById)
 
-async function createBook(){
-    return null;
+function getCurrentDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`; // Format: YYYY-MM-DD
 }
+
+// function getCurrentDate() {
+//     const date = new Date();
+//     date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+//     return date.toISOString().split('T')[0]; // Format: YYYY-MM-DD
+// }
+
+async function createBook(title, author_id, price, genre) {
+    const published_date = getCurrentDate();
+    const [resultCreate] = await pool.query(`
+        INSERT INTO books (title, author_id, price, published_date, genre)
+        VALUES (?, ?, ?, ?, ?)
+    `, [title, author_id, price, published_date, genre]);
+    const id = resultCreate.insertId
+    return getBookById(id)
+    // return resultCreate.insertId;
+}
+
+const resultCreate = await createBook('To Kill a Mockingbird', 1, 15.99, 'Southern Gothic')
+console.log(resultCreate)
